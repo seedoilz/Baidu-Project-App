b2a0192ccb9d535caf0cecaee42f72968011fd4d
seedoilz <seedoilz609@qq.com>
2023-07-13 19:00:23 +0800

    Initial commit

---
+++ app/src/main/java/com/seedoilz/mybrowser/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.seedoilz.mybrowser
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
